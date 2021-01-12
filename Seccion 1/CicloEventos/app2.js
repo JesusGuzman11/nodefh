dcba93f9aa915cf92ae7d9e1b31a555f79589058
Jesus Guzman <jesusguzman.0311@gmail.com>
2021-01-12 14:59:21 -0600

    Comment Events

--- Seccion 1/CicloEventos/app2.js
+++ Seccion 1/CicloEventos/app2.js
@@ -1,3 +1,16 @@
+/* 
+Debido a la pila de procesos de node
+se ejecuta primero el inicio y los timeouts depues
+por que son un proceso async entonces tienen que pasar por
+la pila de apis y de callbacks antes de pasar al main
+por eso es resultado es:
+-Inicio del programa.
+-Fin del programa.
+-Segundo Timeout.
+-Tercer Timeout.
+-Primer Timeout.
+*/
+
 console.log("Inicio del programa");
 
 setTimeout(function () {
